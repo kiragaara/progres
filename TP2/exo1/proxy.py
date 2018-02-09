ead1f07e00c45b2f53e7ba8f8d185c833b52eb0f
CHELBI <3402547@ppti-14-408-15.ufr-info-p6.jussieu.fr>
2018-02-09 18:31:03 +0100

    modif

--- TP2/exo1/proxy.py
+++ TP2/exo1/proxy.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 
 from socket import *
-import os.path
+from sys import argv
 
-    
+ip_server = "127.0.0.1"
+port_ip = "8081"
+
+ip_proxy = "127.0.0.1"
+port_proxy = "8082"
+
+
+
+
 
 if __name__ == '__main__':
+
+    if(len(argv) == 3):
+        ip_server = argv[1]
+        serverPort = int(argv[2])
+        
 
     serverPort = 8081
     serverSocket = socket(AF_INET,SOCK_STREAM)
     serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    serverSocket.bind(("134.157.27.14",serverPort))
-    serverSocket.listen(1)
-    messageOk = "HTTP/1.1 200 OK \r\n\r\n"
-    messagenotFound =  "HTTP/1.1 401 Not Found\r\n\r\n"+" <h1> 404 NOT FOUND  <h1>"
-    while 1 :
+    serverSocket.bind((ip_proxy,port_proxy))
+    serverSocket.listen(6)
 
+
+
+    while 1 :
         try :
             connectionSocket, address = serverSocket.accept()
             print("connection :")
--- TP2/exo1/server.py
+++ TP2/exo1/server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from sys import argv
 from socket import *
+from threading import * 
 import os.path 
 import re
 
@@ -8,20 +9,44 @@ import re
 #default config
 ip_server = "127.0.0.1"
 serverPort = 8081
-file_index = "index.html"
+file_index = "image.html"
 
-folder="folder/index.html"
+folder="folder/"
 blockSendFile = 1024
 
-# message HTTP
+# message HTP
 messageOk = "HTTP/1.1 200 OK \r\n\r\n"
 messagenotFound =  "HTTP/1.1 401 Not Found\r\n\r\n"+" <h1> 404 NOT FOUND  <h1>"
 
 
+def handle_client_proxy(connectionSocket, address):# thread pour chaque connexion du proxy 
+
+    try :
+        message = connectionSocket.recv(1024)
+        print(message)
+        file = get_file_request(message)
+        if exist_file(file) :
+            send_file(connectionSocket,file)
+        else :
+            connectionSocket.send(messagenotFound.encode('utf-8'))
+            connectionSocket.close()
+    except : # je dois enlever raise 
+        if(connectionSocket != None):
+            try :
+                connectionSocket.close()
+            except error :
+                raise 
+        raise
+
+
+
+    
 def exist_file(file):# check if file exist or not
+    if (file == None) :
+        return None
     try:
-        print(file)
         if(os.path.exists(folder+file)) :
+            print("file exist")
             return True
     except:
         raise Exception("file not existe",file)
@@ -30,24 +55,26 @@ def exist_file(file):# check if file exist or not
 def send_file(clientSocket, file):# send file
 
     realFile = folder+file # concatener because i assume that all file are located in folder 'folder = folder\'
-    cpt = 0
     try:
-        size = os.path.getsize(realFile)
-        with open(realFile) as f:
-            while cpt < size :
-                clientSocket.sendall(messageOk.encode('utf-8'))
-                clientSocket.sendall(f.read(blockSendFile).encode('utf-8'))
-                cpt += blockSendFile
+        with open(realFile,"rb") as f:
+            clientSocket.sendall(messageOk.encode('utf-8'))
+            clientSocket.sendall(f.read())
     except :
-        raise Exception("can't open file")
+        raise Exception("can't open file "+realFile)
            
 
+
+    
 def get_file_request(messageClient): # string 
     
     myre = re.match("GET /(.*?)\s",messageClient.decode('utf-8')) ; # regex to get file ,any file
-    if(myre) : 
-        return myre.group(1)
+    if(myre) :
+        if(myre.group(1)):
+            return myre.group(1)
     return file_index # principal page 
+
+
+
 
 if __name__ == '__main__':
 
@@ -66,17 +93,8 @@ if __name__ == '__main__':
         try :
             connectionSocket, address = serverSocket.accept()
             print("connection :")
-            message = connectionSocket.recv(1024)
-            print(message)
-            # file  = message[5:13] # file toto.txt localhost:8080/toto.txt
-            file = get_file_request(message)
-            if exist_file(file) :
-                send_file(connectionSocket,file)
-
-        
-            connectionSocket.close()
-        except:
-            if(connectionSocket != None):
-                connectionSocket.send(messagenotFound.encode('utf-8'))
-                connectionSocket.close()
-                raise # for debug je dois l enlever a la fin 
+            Thread(target=handle_client_proxy, args=(connectionSocket,address, )).start()
+        except KeyboardInterrupt :
+            raise
+        except error as e  :
+            print(e)
